f937328d42b70d6ee16a3b9e27f70e495d53a146
Jacob Witte <wittejg@gmail.com>
2023-02-24 11:07:33 -0500

    Add project files.

---
+++ test-function/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using test_function;
+
+//Testing AFunction() and catching thrown exceptoins
+foreach(var i in new int[] { int.MinValue, -11, -0, -9, -15, -5, -1, 0, 
+    1, 4, 5, 6, 2, 10, int.MaxValue }) { 
+
+try {
+    Console.WriteLine(NewFunction.AFunction(i));
+}
+catch {
+    Console.WriteLine("Threw and exception!");
+}
+}
